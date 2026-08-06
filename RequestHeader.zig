b44b11f4b872a4805903519cb76801f1ee12ce4a
Daniel Doža <doza.daniel@gmail.com>
2026-08-07 00:39:38 +0200

    Split requestheader and apiversionsreq to diff files

---
+++ RequestHeader.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+const Reader = @import("Reader.zig");
+
+const mem = std.mem;
+
+const RequestHeader = @This();
+
+request_api_key: i16 = 0,
+request_api_version: i16 = 0,
+correlation_id: i32 = 0,
+client_id: ?[]const u8 = null,
+
+pub fn read(self: *RequestHeader, reader: *Reader, alloc: mem.Allocator) !void {
+    self.request_api_key = try reader.readInt(i16);
+    self.request_api_version = try reader.readInt(i16);
+    self.correlation_id = try reader.readInt(i32);
+    self.client_id = try reader.readNullableString(alloc);
+    if (try reader.readUvarint() > 0) {
+        return error.UnexpectedTags;
+    }
+}
+
+pub fn deinit(self: *RequestHeader, alloc: mem.Allocator) void {
+    if (self.client_id) |str| {
+        alloc.free(str);
+    }
+}
