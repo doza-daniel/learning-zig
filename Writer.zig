643ce0deff343bbda2730fb037999940964ecda1
Daniel Doža <doza.daniel@gmail.com>
2026-08-07 15:49:00 +0200

    Switch to arraylist for buffer

--- Writer.zig
+++ Writer.zig
@@ -4,34 +4,29 @@ const mem = std.mem;
 
 const Writer = @This();
 
-buf: []u8 = &[_]u8{},
-offset: usize = 0,
-max_size: usize = 10 * 1024 * 1024 * 1024, // 10 MiB
+buf: std.ArrayList(u8) = .empty,
 
 pub fn writeInt(self: *Writer, alloc: mem.Allocator, comptime T: type, val: T) !void {
-    try self.resizeIfNeeded(alloc, @sizeOf(T));
-    const b = self.buf[self.offset..][0..@sizeOf(T)];
-    mem.writeInt(T, b, val, .big);
-    self.offset += @sizeOf(T);
+    var buffer = [_]u8{0} ** @sizeOf(T);
+    mem.writeInt(T, &buffer, val, .big);
+    try self.buf.appendSlice(alloc, &buffer);
 }
 
 pub fn writeUvarint(self: *Writer, alloc: mem.Allocator, val: u32) !void {
-    _ = self;
-    var al: std.ArrayList(u8) = .empty;
-    defer al.deinit(alloc);
-
     var tmp = val;
-    while (true) {
-        if (al.items.len > 0) {
-            al.items[al.items.len - 1] |= 0x80; // set continuation bit
+    var buffer = [_]u8{0} ** 5;
+    var i: u8 = 0;
+    while (i < buffer.len) : (i += 1) {
+        if (i > 0) {
+            buffer[i - 1] |= 0x80; // set continuation bit
         }
-        try al.append(alloc, @as(u8, @truncate(tmp)) & 0x7F);
+        buffer[i] = @as(u8, @truncate(tmp)) & 0x7F;
         tmp >>= 7;
         if (tmp == 0) {
             break;
         }
     }
-    std.debug.print("{x}\n", .{al.items});
+    try self.buf.appendSlice(alloc, buffer[0 .. i + 1]);
 }
 
 pub fn writeString() !void {}
@@ -39,21 +34,7 @@ pub fn writeString() !void {}
 pub fn writeCompactString() !void {}
 
 pub fn deinit(self: *Writer, alloc: mem.Allocator) void {
-    alloc.free(self.buf);
-}
-
-fn resizeIfNeeded(self: *Writer, alloc: mem.Allocator, want: usize) !void {
-    const need = self.offset + want;
-    if (self.buf.len >= need) {
-        return;
-    }
-    if (need > self.max_size) {
-        return error.BufferOutOfSpace;
-    }
-    const resized = try alloc.alloc(u8, need);
-    @memcpy(resized[0..self.buf.len], self.buf);
-    alloc.free(self.buf);
-    self.buf = resized;
+    self.buf.deinit(alloc);
 }
 
 test "writeInt" {
@@ -67,36 +48,16 @@ test "writeInt" {
         0x3C, 0xF3, // 15603
         0x5D, 0x00, 0x79, 0xF8, // 1560312312
     };
-    try std.testing.expect(mem.eql(u8, &expect, w.buf));
-}
-
-test "writeInt:out_of_space" {
-    const max_size: usize = 10;
-    const mock = try std.testing.allocator.alloc(u8, max_size - 1);
-    defer std.testing.allocator.free(mock);
-    var w = Writer{
-        .buf = mock,
-        .offset = max_size - 1,
-        .max_size = max_size,
-    };
-    w.writeInt(std.testing.allocator, u16, 15603) catch |err| {
-        try std.testing.expectEqual(error.BufferOutOfSpace, err);
-        return;
-    };
-    try std.testing.expect(false);
-}
-
-test "writeInt:no_resize" {
-    var buf = [_]u8{0};
-    var w = Writer{
-        .buf = &buf,
-    };
-    try w.writeInt(std.testing.failing_allocator, u8, 15);
-    const expect = [_]u8{0x0F};
-    try std.testing.expect(mem.eql(u8, &expect, w.buf));
+    try std.testing.expect(mem.eql(u8, &expect, w.buf.items));
 }
 
 test "writeUvarint" {
     var w = Writer{};
-    try w.writeUvarint(std.testing.allocator, 0xffffffff);
+    const expect: u32 = 0xffff;
+    try w.writeUvarint(std.testing.allocator, expect);
+    const Reader = @import("Reader.zig");
+    var x: Reader = .{ .src = w.buf.items };
+    const got = try x.readUvarint();
+    try std.testing.expectEqual(got, expect);
+    w.deinit(std.testing.allocator);
 }
