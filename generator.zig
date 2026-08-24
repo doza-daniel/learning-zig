9dccf377510bf3f9c7fa36a4fab3a9d30a4e8d6a
Daniel Doža <doza.daniel@gmail.com>
2026-08-24 23:32:20 +0200

    Generator works somewhat

--- generator.zig
+++ generator.zig
@@ -4,7 +4,13 @@ const json = std.json;
 const mem = std.mem;
 
 pub fn main(init: std.process.Init) !void {
-    try hello(init);
+    const paths = [_][]const u8{
+        "./protocol_json_files/ProduceRequest.json",
+    };
+
+    for (paths) |path| {
+        try readAndPrint(init, path);
+    }
 }
 
 const Field = struct {
@@ -64,16 +70,6 @@ const Type = enum {
     Data,
 };
 
-pub fn hello(init: std.process.Init) !void {
-    const paths = [_][]const u8{
-        "./protocol_json_files/ProduceResponse.json",
-    };
-
-    for (paths) |path| {
-        try readAndPrint(init, path);
-    }
-}
-
 fn readAndPrint(init: std.process.Init, path: []const u8) !void {
     const file_content = try std.Io.Dir.cwd().readFileAlloc(init.io, path, init.gpa, .unlimited);
     defer init.gpa.free(file_content);
@@ -84,33 +80,35 @@ fn readAndPrint(init: std.process.Init, path: []const u8) !void {
     const parsed = try json.parseFromSlice(Msg, init.gpa, clean, .{ .ignore_unknown_fields = true });
     defer parsed.deinit();
 
-    // std.json.fmt(parsed.value, .{});
-    // std.debug.print("{f}\n", .{std.json.fmt(parsed.value, .{})});
-
-    try p(parsed.value, init.gpa);
+    try codeGen(parsed.value, init.gpa);
 }
 
-fn p(T: anytype, alloc: std.mem.Allocator) !void {
-    const tinfo = try gen(T.type, alloc);
+fn codeGen(T: anytype, alloc: std.mem.Allocator) !void {
+    const tinfo = try parseTypeInfo(T.type, alloc);
     defer tinfo.deinit(alloc);
+
+    var q: std.Deque(Field) = .empty;
+    defer q.deinit(alloc);
+
+    var close: bool = false;
 
     switch (tinfo.t) {
         .Request, .Response, .Data => {
             std.debug.print("const std = @import(\"std\");\n", .{});
             std.debug.print("const Reader = @import(\"Reader.zig\");\n", .{});
+            std.debug.print("const RecordBatch = @import(\"RecordBatch.zig\");\n", .{});
             std.debug.print("const {s} = @This();\n", .{T.name});
         },
         .Struct => {
-                // push to queue
-            std.debug.print("const {s} = struct{{\n", .{T.name});
-            std.debug.print("}}\n", .{}); // TODO: remove
+            std.debug.print("const {s} = struct{{\n", .{tinfo.name.?});
+            close = true;
         },
         .Array => {
             if (tinfo.sub) |sub| {
                 if (sub.t == .Struct) {
                     // push to queue
-                    std.debug.print("const {s} = struct{{\n", .{T.name});
-                    std.debug.print("}}\n", .{}); // TODO: remove
+                    std.debug.print("const {s} = struct{{\n", .{sub.name.?});
+                    close = true;
                 }
             }
         },
@@ -118,66 +116,82 @@ fn p(T: anytype, alloc: std.mem.Allocator) !void {
     }
 
     for (T.fields) |field| {
-        const finfo = try gen(field.type, alloc);
+        const finfo = try parseTypeInfo(field.type, alloc);
+        defer finfo.deinit(alloc);
+        switch (finfo.t) {
+            .Struct, .Array => try q.pushBack(alloc, field),
+            else => {},
+        }
         std.debug.print("{s}: {s},\n", .{ field.name, finfo.name.? });
-        finfo.deinit(alloc);
     }
     std.debug.print("pub fn read(self: *@This(), reader: *Reader, alloc: std.mem.Allocator) !void {{\n", .{});
     for (T.fields) |field| {
-        const finfo = try gen(field.type, alloc);
+        const finfo = try parseTypeInfo(field.type, alloc);
         defer finfo.deinit(alloc);
 
-        std.debug.print("\t", .{});
         switch (finfo.t) {
             .Boolean => {
-                std.debug.print("self.{s} = try reader.readBool();", .{field.name});
+                std.debug.print("self.{s} = try reader.readBool();\n", .{field.name});
             },
             .Int8 => {
-                std.debug.print("self.{s} = try reader.readInt(i8);", .{field.name});
+                std.debug.print("self.{s} = try reader.readInt(i8);\n", .{field.name});
             },
             .Int16 => {
-                std.debug.print("self.{s} = try reader.readInt(i16);", .{field.name});
+                std.debug.print("self.{s} = try reader.readInt(i16);\n", .{field.name});
             },
             .Int32 => {
-                std.debug.print("self.{s} = try reader.readInt(i32);", .{field.name});
+                std.debug.print("self.{s} = try reader.readInt(i32);\n", .{field.name});
             },
             .Int64 => {
-                std.debug.print("self.{s} = try reader.readInt(i64);", .{field.name});
+                std.debug.print("self.{s} = try reader.readInt(i64);\n", .{field.name});
             },
             .Uint16 => {
-                std.debug.print("self.{s} = try reader.readInt(u16);", .{field.name});
+                std.debug.print("self.{s} = try reader.readInt(u16);\n", .{field.name});
             },
             .Uint32 => {
-                std.debug.print("self.{s} = try reader.readInt(u32);", .{field.name});
+                std.debug.print("self.{s} = try reader.readInt(u32);\n", .{field.name});
             },
             .UUID => {
-                std.debug.print("self.{s} = try reader.readUuid();", .{field.name});
+                std.debug.print("self.{s} = try reader.readUuid();\n", .{field.name});
             },
             .Float64 => {
-                std.debug.print("self.{s} = try reader.readFloat64();", .{field.name});
+                std.debug.print("self.{s} = try reader.readFloat64();\n", .{field.name});
             },
             .String => {
-                std.debug.print("self.{s} = try reader.readString(alloc);", .{field.name});
+                std.debug.print("self.{s} = try reader.readString(alloc);\n", .{field.name});
             },
             .Bytes => {
-                std.debug.print("self.{s} = try reader.readBytes(alloc);", .{field.name});
+                std.debug.print("self.{s} = try reader.readBytes(alloc);\n", .{field.name});
             },
             .Array => {
-                std.debug.print("self.{s} = try reader.readArray({s}, alloc);", .{ finfo.sub.?.name.?, field.name });
+                std.debug.print("self.{s} = try reader.readArray({s}, alloc);\n", .{ field.name, finfo.sub.?.name.? });
             },
             .Struct => {
-                std.debug.print("try self.{s}.read(reader, alloc);", .{field.name});
+                std.debug.print("try self.{s}.read(reader, alloc);\n", .{field.name});
             },
-            .Records => unreachable,
+            .Records => {
+                std.debug.print("var bytes = try reader.readBytes(alloc);\n", .{});
+                std.debug.print("defer alloc.free(bytes);\n", .{});
+                std.debug.print("var record_reader: Reader = .{{.src = &bytes}};\n", .{});
+                std.debug.print("try self.{s}.read(&record_reader, alloc);\n", .{field.name});
+            },
 
+            // TODO: fix
             .Response, .Request, .Data => unreachable,
         }
-        std.debug.print("\n", .{});
     }
     std.debug.print("}}\n", .{});
+
+    if (close) {
+        std.debug.print("}};\n", .{});
+    }
+
+    while (q.len > 0) {
+        try codeGen(q.popFront().?, alloc);
+    }
 }
 
-fn gen(s: []const u8, alloc: std.mem.Allocator) !*typeInfo {
+fn parseTypeInfo(s: []const u8, alloc: std.mem.Allocator) !*typeInfo {
     const result = try alloc.create(typeInfo);
     result.t = try parseType(s);
     result.name = null;
@@ -185,7 +199,7 @@ fn gen(s: []const u8, alloc: std.mem.Allocator) !*typeInfo {
 
     switch (result.t) {
         .Array => {
-            result.sub = try gen(s[2..], alloc);
+            result.sub = try parseTypeInfo(s[2..], alloc);
             result.name = try std.fmt.allocPrint(alloc, "[]{s}", .{result.sub.?.name.?});
         },
 
@@ -201,40 +215,38 @@ fn gen(s: []const u8, alloc: std.mem.Allocator) !*typeInfo {
         .UUID => result.name = "[16]u8",
         .Float64 => result.name = "f64",
         .String => result.name = "[]const u8",
+        .Bytes => result.name = "[]u8",
+        .Records => result.name = "RecordBatch",
 
         .Request => result.name = "request",
         .Response => result.name = "response",
         .Data => result.name = "data",
-
-        .Bytes, .Records => unreachable,
     }
     return result;
 }
 
-fn parseType(typeStr: []const u8) !Type {
-    const map = std.static_string_map.StaticStringMap(Type);
-    const kv = struct { []const u8, Type };
-    const slice: []const kv = &.{
-        .{ "bool", .Boolean },
-        .{ "int8", .Int8 },
-        .{ "int16", .Int16 },
-        .{ "uint16", .Uint16 },
-        .{ "int32", .Int32 },
-        .{ "uint32", .Uint32 },
-        .{ "int64", .Int64 },
-        .{ "float64", .Float64 },
-        .{ "string", .String },
-        .{ "uuid", .UUID },
-        .{ "bytes", .Bytes },
-        .{ "records", .Records },
-        .{ "struct", .Struct },
+const typeMap: std.static_string_map.StaticStringMap(Type) = .initComptime(&[_]struct { []const u8, Type }{
+    .{ "bool", .Boolean },
+    .{ "int8", .Int8 },
+    .{ "int16", .Int16 },
+    .{ "uint16", .Uint16 },
+    .{ "int32", .Int32 },
+    .{ "uint32", .Uint32 },
+    .{ "int64", .Int64 },
+    .{ "float64", .Float64 },
+    .{ "string", .String },
+    .{ "uuid", .UUID },
+    .{ "bytes", .Bytes },
+    .{ "records", .Records },
+    .{ "struct", .Struct },
 
-        .{ "request", .Request },
-        .{ "response", .Response },
-        .{ "data", .Data },
-    };
-    const m = map.initComptime(slice);
-    if (m.get(typeStr)) |typ| {
+    .{ "request", .Request },
+    .{ "response", .Response },
+    .{ "data", .Data },
+});
+
+fn parseType(typeStr: []const u8) !Type {
+    if (typeMap.get(typeStr)) |typ| {
         return typ;
     } else {
         if (typeStr[0] == '[') {
