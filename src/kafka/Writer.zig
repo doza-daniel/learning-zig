be9593eb51dba568b0f085efd81cc5580326f22a
Daniel Doza <doza.daniel@gmail.com>
2026-09-09 15:19:31 +0200

    wip - rewriting all Writer tests

--- src/kafka/Writer.zig
+++ src/kafka/Writer.zig
@@ -95,17 +95,17 @@ pub fn writeCompactNullableString(self: *Writer, alloc: mem.Allocator, val: ?[]c
     }
 }
 
-pub fn writeBytes(self: *Writer, alloc: mem.Allocator, val: []u8) !void {
-    try self.writeInt(alloc, i32, val.len);
-    try self.buf.appendSlice(val);
-}
-
-pub fn writeCompactBytes(self: *Writer, alloc: mem.Allocator, val: []u8) !void {
-    try self.writeUvarint(alloc, val.len + 1);
+pub fn writeBytes(self: *Writer, alloc: mem.Allocator, val: []const u8) !void {
+    try self.writeInt(alloc, i32, @intCast(val.len));
     try self.buf.appendSlice(alloc, val);
 }
 
-pub fn writeNullableBytes(self: *Writer, alloc: mem.Allocator, val: ?[]u8) !void {
+pub fn writeCompactBytes(self: *Writer, alloc: mem.Allocator, val: []const u8) !void {
+    try self.writeUvarint(alloc, @intCast(val.len + 1));
+    try self.buf.appendSlice(alloc, val);
+}
+
+pub fn writeNullableBytes(self: *Writer, alloc: mem.Allocator, val: ?[]const u8) !void {
     if (val) |bs| {
         try self.writeBytes(alloc, bs);
     } else {
@@ -113,7 +113,7 @@ pub fn writeNullableBytes(self: *Writer, alloc: mem.Allocator, val: ?[]u8) !void
     }
 }
 
-pub fn writeCompactNullableBytes(self: *Writer, alloc: mem.Allocator, val: ?[]u8) !void {
+pub fn writeCompactNullableBytes(self: *Writer, alloc: mem.Allocator, val: ?[]const u8) !void {
     if (val) |bs| {
         try self.writeCompactBytes(alloc, bs);
     } else {
@@ -121,12 +121,12 @@ pub fn writeCompactNullableBytes(self: *Writer, alloc: mem.Allocator, val: ?[]u8
     }
 }
 
-pub fn writeArray(self: *Writer, alloc: mem.Allocator, val: []type, version: i16) !void {
-    try self.writeInt(alloc, i32, val.len);
+pub fn writeArray(self: *Writer, alloc: mem.Allocator, T: type, val: []const T, version: i16) !void {
+    try self.writeInt(alloc, i32, @intCast(val.len));
     for (val) |item| {
-        switch (@typeInfo(item)) {
+        switch (@typeInfo(T)) {
             .int => {
-                try self.writeInt(alloc, @TypeOf(item), item);
+                try self.writeInt(alloc, T, item);
             },
             .@"struct" => {
                 try item.write(alloc, self, version);
@@ -139,12 +139,12 @@ pub fn writeArray(self: *Writer, alloc: mem.Allocator, val: []type, version: i16
     }
 }
 
-pub fn writeCompactArray(self: *Writer, alloc: mem.Allocator, T: type, val: []T, version: i16) !void {
-    try self.writeUvarint(alloc, val.len + 1);
+pub fn writeCompactArray(self: *Writer, alloc: mem.Allocator, T: type, val: []const T, version: i16) !void {
+    try self.writeUvarint(alloc, @intCast(val.len + 1));
     for (val) |item| {
-        switch (@typeInfo(item)) {
+        switch (@typeInfo(T)) {
             .int => {
-                try self.writeInt(alloc, @TypeOf(item), item);
+                try self.writeInt(alloc, T, item);
             },
             .@"struct" => {
                 try item.write(alloc, self, version);
@@ -186,81 +186,433 @@ pub fn deinit(self: *Writer, alloc: mem.Allocator) void {
     self.buf.deinit(alloc);
 }
 
-test "writeInt" {
-    var w = Writer{};
-    try w.writeInt(std.testing.allocator, u8, 15);
-    try w.writeInt(std.testing.allocator, u16, 15603);
-    try w.writeInt(std.testing.allocator, u32, 1560312312);
-    defer w.deinit(std.testing.allocator);
-    const expect = [_]u8{
-        0x0F, // 15
-        0x3C, 0xF3, // 15603
-        0x5D, 0x00, 0x79, 0xF8, // 1560312312
-    };
-    try std.testing.expect(mem.eql(u8, &expect, w.buf.items));
-}
+test "writeBool" {
+    const gpa = std.testing.allocator;
 
-test "writeUvarint" {
-    var w = Writer{};
-    const expect: u32 = 0xffff;
-    try w.writeUvarint(std.testing.allocator, expect);
-    const Reader = @import("Reader.zig");
-    var x: Reader = .{ .src = w.buf.items };
-    const got = try x.readUvarint();
-    try std.testing.expectEqual(got, expect);
-    w.deinit(std.testing.allocator);
-}
-
-test "writeNullableString" {
     const table = .{
-        .{ .input = null, .expect = [_]u8{ 0xff, 0xff } },
-        .{ .input = "", .expect = [_]u8{ 0x00, 0x00 } },
-        .{ .input = "a", .expect = [_]u8{ 0x00, 0x01, 'a' } },
-        .{ .input = "aa", .expect = [_]u8{ 0x00, 0x02, 'a', 'a' } },
-        .{ .input = "a" ** 256, .expect = [_]u8{ 0x01, 0x00 } ++ [_]u8{'a'} ** 256 },
+        .{ .in = true, .expect = .{0x01} },
+        .{ .in = false, .expect = .{0x00} },
     };
     inline for (table) |case| {
         var w: Writer = .{};
-        defer w.deinit(std.testing.allocator);
+        defer w.deinit(gpa);
 
-        try w.writeNullableString(std.testing.allocator, case.input);
+        try w.writeBool(gpa, case.in);
+        try std.testing.expectEqualSlices(u8, &case.expect, w.buf.items);
+    }
+}
 
-        var expect = case.expect;
-        try std.testing.expectEqualDeep(&expect, w.buf.items);
+test "writeInt" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .t = u8, .in = 0x42, .expect = .{0x42} },
+        .{ .t = u16, .in = 0x1234, .expect = .{ 0x12, 0x34 } },
+        .{ .t = u32, .in = 0x12345678, .expect = .{ 0x12, 0x34, 0x56, 0x78 } },
+        .{ .t = u64, .in = 0x123456789ABCDEF1, .expect = .{ 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF1 } },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeInt(gpa, case.t, case.in);
+        try std.testing.expectEqualSlices(u8, &case.expect, w.buf.items);
+    }
+}
+
+test "writeVarint" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = std.math.minInt(i32) },
+        .{ .in = -123456 },
+        .{ .in = -123457 },
+        .{ .in = -2 },
+        .{ .in = -1 },
+        .{ .in = 0 },
+        .{ .in = 1 },
+        .{ .in = 2 },
+        .{ .in = 123456 },
+        .{ .in = 123457 },
+        .{ .in = std.math.maxInt(i32) },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeVarint(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        try std.testing.expectEqual(case.in, try r.readVarint());
+    }
+}
+
+test "writeVarlong" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = std.math.minInt(i64) },
+        .{ .in = -123456 },
+        .{ .in = -123457 },
+        .{ .in = -2 },
+        .{ .in = -1 },
+        .{ .in = 0 },
+        .{ .in = 1 },
+        .{ .in = 2 },
+        .{ .in = 123456 },
+        .{ .in = 123457 },
+        .{ .in = std.math.maxInt(i64) },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeVarlong(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        try std.testing.expectEqual(case.in, try r.readVarlong());
+    }
+}
+
+test "writeUvarint" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = 0 },
+        .{ .in = 1 },
+        .{ .in = 2 },
+        .{ .in = 3 },
+        .{ .in = 123456 },
+        .{ .in = 123457 },
+        .{ .in = std.math.maxInt(u32) },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeUvarint(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        try std.testing.expectEqual(case.in, try r.readUvarint());
+    }
+}
+
+test "writeUvarlong" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = 0 },
+        .{ .in = 1 },
+        .{ .in = 2 },
+        .{ .in = 3 },
+        .{ .in = 123456 },
+        .{ .in = 123457 },
+        .{ .in = std.math.maxInt(u64) },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeUvarlong(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        try std.testing.expectEqual(case.in, try r.readUvarlong());
+    }
+}
+
+test "writeUuid" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = [_]u8{0} ** 16 },
+        .{ .in = [16]u8{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 } },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeUuid(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        try std.testing.expectEqual(case.in, try r.readUuid());
+    }
+}
+
+test "writeFloat64" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = 0.0 },
+        .{ .in = 0.1 },
+        .{ .in = 1.23 },
+        .{ .in = std.math.floatMax(f64) },
+        .{ .in = std.math.floatMin(f64) },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeFloat64(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        try std.testing.expectEqual(case.in, try r.readFloat64());
+    }
+}
+
+test "writeString" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = "" },
+        .{ .in = "a" },
+        .{ .in = "foo" },
+        .{ .in = "foobar" },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeString(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readString(gpa);
+        defer gpa.free(result);
+        try std.testing.expectEqualStrings(case.in, result);
     }
 }
 
 test "writeCompactString" {
+    const gpa = std.testing.allocator;
+
     const table = .{
-        .{ .input = "", .expect = [_]u8{0x01} },
-        .{ .input = "a", .expect = [_]u8{ 0x02, 'a' } },
-        .{ .input = "aa", .expect = [_]u8{ 0x03, 'a', 'a' } },
-        .{ .input = "aaa", .expect = [_]u8{ 0x04, 'a', 'a', 'a' } },
-        .{ .input = "a" ** 130, .expect = [_]u8{ 0x83, 0x01 } ++ [_]u8{'a'} ** 130 },
+        .{ .in = "" },
+        .{ .in = "a" },
+        .{ .in = "foo" },
+        .{ .in = "foobar" },
     };
     inline for (table) |case| {
         var w: Writer = .{};
-        defer w.deinit(std.testing.allocator);
+        defer w.deinit(gpa);
 
-        try w.writeCompactString(std.testing.allocator, case.input);
-
-        var expect = case.expect;
-        try std.testing.expectEqualDeep(&expect, w.buf.items);
+        try w.writeCompactString(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readCompactString(gpa);
+        defer gpa.free(result);
+        try std.testing.expectEqualStrings(case.in, result);
     }
 }
 
-test "writeBool:true" {
-    var w: Writer = .{};
-    defer w.deinit(std.testing.allocator);
-    try w.writeBool(std.testing.allocator, true);
-    try std.testing.expect(mem.eql(u8, w.buf.items, &.{0x01}));
+test "writeNullableString" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = @as(?[]const u8, null) },
+        .{ .in = @as(?[]const u8, "foobar") },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeNullableString(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readNullableString(gpa);
+        if (case.in) |str| {
+            defer gpa.free(result.?);
+            try std.testing.expectEqualStrings(str, result.?);
+        } else {
+            try std.testing.expectEqual(null, result);
+        }
+    }
 }
 
-test "writeBool:false" {
-    var w: Writer = .{};
-    defer w.deinit(std.testing.allocator);
-    try w.writeBool(std.testing.allocator, false);
-    try std.testing.expect(mem.eql(u8, w.buf.items, &.{0x00}));
+test "writeCompactNullableString" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = @as(?[]const u8, null) },
+        .{ .in = @as(?[]const u8, "foobar") },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeCompactNullableString(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readCompactNullableString(gpa);
+        if (case.in) |str| {
+            defer gpa.free(result.?);
+            try std.testing.expectEqualStrings(str, result.?);
+        } else {
+            try std.testing.expectEqual(null, result);
+        }
+    }
+}
+
+test "writeBytes" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = .{} },
+        .{ .in = .{0x0} },
+        .{ .in = .{ 0x01, 0x02, 0x03 } },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeBytes(gpa, &case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readBytes(gpa);
+        defer gpa.free(result);
+        try std.testing.expectEqualSlices(u8, &case.in, result);
+    }
+}
+
+test "writeCompactBytes" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = .{} },
+        .{ .in = .{0x0} },
+        .{ .in = .{ 0x01, 0x02, 0x03 } },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeCompactBytes(gpa, &case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readCompactBytes(gpa);
+        defer gpa.free(result);
+        try std.testing.expectEqualSlices(u8, &case.in, result);
+    }
+}
+
+test "writeNullableBytes" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = @as(?[]const u8, null) },
+        .{ .in = @as(?[]const u8, &.{ 0x01, 0x02, 0x03 }) },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeNullableBytes(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readNullableBytes(gpa);
+        if (case.in) |str| {
+            defer gpa.free(result.?);
+            try std.testing.expectEqualStrings(str, result.?);
+        } else {
+            try std.testing.expectEqual(null, result);
+        }
+    }
+}
+
+test "writeCompactNullableBytes" {
+    const gpa = std.testing.allocator;
+
+    const table = .{
+        .{ .in = @as(?[]const u8, null) },
+        .{ .in = @as(?[]const u8, &.{ 0x01, 0x02, 0x03 }) },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeCompactNullableBytes(gpa, case.in);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readCompactNullableBytes(gpa);
+        if (case.in) |str| {
+            defer gpa.free(result.?);
+            try std.testing.expectEqualStrings(str, result.?);
+        } else {
+            try std.testing.expectEqual(null, result);
+        }
+    }
+}
+
+test "writeArray" {
+    const gpa = std.testing.allocator;
+
+    const mock = struct {
+        f: i32,
+        pub fn write(self: @This(), alloc: mem.Allocator, w: *Writer, _: i16) !void {
+            try w.writeInt(alloc, @TypeOf(self.f), self.f);
+        }
+
+        pub fn read(self: *@This(), r: *@import("Reader.zig"), _: mem.Allocator, _: i16) !void {
+            self.f = try r.readInt(@TypeOf(self.f));
+        }
+    };
+
+    const table = .{
+        .{
+            .t = mock,
+            .in = .{
+                mock{ .f = 0x12345678 },
+                mock{ .f = 0x77654321 },
+            },
+        },
+        .{ .t = i32, .in = .{ 0x12345678, 0x77654321 } },
+        .{ .t = []const u8, .in = .{ "foo", "bar" } },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeArray(gpa, case.t, &case.in, 0);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readArray(case.t, gpa, 0);
+        defer {
+            if (case.t == []const u8) {
+                for (result) |str| {
+                    gpa.free(str);
+                }
+            }
+            gpa.free(result);
+        }
+        try std.testing.expectEqualDeep(@as([]const case.t, &case.in), result);
+    }
+}
+
+test "writeCompactArray" {
+    const gpa = std.testing.allocator;
+
+    const mock = struct {
+        f: i32,
+        pub fn write(self: @This(), alloc: mem.Allocator, w: *Writer, _: i16) !void {
+            try w.writeInt(alloc, @TypeOf(self.f), self.f);
+        }
+
+        pub fn read(self: *@This(), r: *@import("Reader.zig"), _: mem.Allocator, _: i16) !void {
+            self.f = try r.readInt(@TypeOf(self.f));
+        }
+    };
+
+    const table = .{
+        .{
+            .t = mock,
+            .in = .{
+                mock{ .f = 0x12345678 },
+                mock{ .f = 0x77654321 },
+            },
+        },
+        .{ .t = i32, .in = .{ 0x12345678, 0x77654321 } },
+        .{ .t = []const u8, .in = .{ "foo", "bar" } },
+    };
+    inline for (table) |case| {
+        var w: Writer = .{};
+        defer w.deinit(gpa);
+
+        try w.writeCompactArray(gpa, case.t, &case.in, 0);
+        var r: @import("Reader.zig") = .{ .src = w.buf.items };
+        const result = try r.readCompactArray(case.t, gpa, 0);
+        defer {
+            if (case.t == []const u8) {
+                for (result) |str| {
+                    gpa.free(str);
+                }
+            }
+            gpa.free(result);
+        }
+        try std.testing.expectEqualDeep(@as([]const case.t, &case.in), result);
+    }
 }
 
 test "writeNullableStruct" {
